63e9c847ebf912c50cb2b2b0d6f3e5695a2552f7
Gorlaks <imgorlaks@gmail.com>
2020-11-09 21:47:01 +0300

    response type

--- canvas/modules/routers/auth.py
+++ canvas/modules/routers/auth.py
@@ -1,6 +1,7 @@
 from fastapi import APIRouter
 
 from canvas.models.auth import User
+from canvas.models.response import Response
 from canvas.modules.auth.auth_service import AuthService
 
 router = APIRouter()
@@ -8,9 +9,9 @@ router = APIRouter()
 auth_service = AuthService()
 
 @router.post("/login")
-def login(user_data: User) -> User:
+def login(user_data: User) -> Response:
   return auth_service.login(user_data.login, user_data.password)
 
 @router.post("/registration")
-def registration(user_data: User) -> User:
+def registration(user_data: User) -> Response:
   return auth_service.registration(user_data)
