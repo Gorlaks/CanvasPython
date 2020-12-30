6df0a6bd1b7753b922f602c7fe2f21b4fa7c88a3
Gorlaks <gorlakswork@gmail.com>
2020-12-30 17:45:29 +0300

    code to allow relative paths for test

--- canvas/tests/test_utils.py
+++ canvas/tests/test_utils.py
@@ -1,11 +1,27 @@
 import unittest
+import sys
+from pathlib import Path
 
-# from canvas.utils.hashing import hash_password
+if __name__ == '__main__' and __package__ is None:
+    file = Path(__file__).resolve()
+    parent, top = file.parent, file.parents[3]
+
+    sys.path.append(str(top))
+    try:
+        sys.path.remove(str(parent))
+    except ValueError: # Already removed
+        pass
+
+    __package__ = 'CanvasPython.canvas.utils'
+
+from ..utils.hashing import hash_password
+
 
 class TestUtils(unittest.TestCase):
 
-  def test_hash_password(self):
-    self.assertEqual(2, 2)
+    def test_hash_password(self):
+        self.assertEqual(2, 2)
 
 
-unittest.main()
+if __name__ == "__main__":
+    unittest.main()
--- test.py
+++
@@ -1,6 +0,0 @@
-from canvas.tests.test_utils import TestUtils
-
-
-if __name__ == "__main__":
-    test_utils = TestUtils()
-    test_utils.test_hash_password()
